a3054c953ef14bb4baacfdf2e64cbb25b2841255
Francisco Pessano <franpessano1@gmail.com>
2021-11-23 22:09:56 -0300

    Messages modularizated

---
+++ scripts/messagesList.js
@@ -0,0 +1,112 @@
+const messagesList = {
+
+    welcome: {
+
+        english: 'Welcome to the Rock Paper and Scissors CLI game!',
+        spanish: '¡Bienvenido al juego de Piedra Papel o Tijeras desde la consola!'
+    },
+
+    gameTitle: {
+
+        english: 'Rock Paper and Scissors CLI game',
+        spanish: 'Piedra Papel o Tijeras desde la consola'        
+    },
+
+    play: {
+
+        english: 'Select one option:',
+        spanish: 'Selecciona una opción'
+    },
+
+    options: {
+
+        english: ['Rock', 'Paper', 'Scissors'],
+        spanish: ['Piedra', 'Papel', 'Tijeras']
+    },
+
+    resultTitle: {
+
+        english: 'Results:',
+        spanish: 'Resultados:'
+    },
+
+    tie: {
+
+        english: '  - The result is a draw! GG',
+        spanish: '  - Empate! Bien Jugado.'
+    },
+
+    victory: {
+
+        english: '  - You win!',
+        spanish: '  - Ganaste!'
+    },
+
+    defeat: {
+
+        english: '  - You lost...',
+        spanish: '  - Perdiste...'
+    },
+
+    computerSelectionTitle: {
+
+        english: 'Computer selection:',
+        spanish: 'Selección de la computadora:'
+    },
+
+    computerSelection: {
+
+        english: '  - Computer selection is ',
+        spanish: '  - La selección de la computadora es '
+    },
+
+    stats: {
+
+        title: {
+
+            english: 'Stats:',
+            spanish: 'Estadisticas:'
+        },
+
+        victory: {
+
+            english: '  - Victory count: ',
+            spanish: '  - Victorias: '
+        },
+
+        consecutiveVictory: {
+
+            english: '      - Consecutive victory count: ',
+            spanish: '      - Victorias consecutivas: ',
+        },
+
+        defeat: {
+
+            english: '  - Defeat count: ',
+            spanish: '  - Derrotas: ',
+        },
+    },
+
+    playAgain: {
+
+        title: {
+
+            english: 'Do you wanna play again?',
+            spanish: '¿Quieres jugar otra vez?'
+        },
+
+        options: {
+
+            english: ['Yes', 'No'],
+            spanish: ['Sí', 'No']
+        }
+    },
+
+    thanksForPlay: {
+
+        english: 'Thanks for playing! Francisco Pessano',
+        spanish: 'Gracias por jugar! Francisco Pessano'
+    }
+}
+
+module.exports = messagesList
